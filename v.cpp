b4291497bda1a7a08fa443196bcb211f8d372c9c
Ignas Biekša <75567342+swoogie@users.noreply.github.com>
2021-02-05 12:57:49 +0200

    Add files via upload

---
+++ v.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Lithuanian");
+    string vardas;
+    cin >> vardas;
+    string eil1;
+    string eil2="*";
+    string eil3;
+    string eil4="*";
+    string eil5;
+    if(vardas.back()=='e' or vardas.back()=='a')
+    eil3 = "* Sveika, " + vardas + "! *";
+    else
+    eil3 = "* Sveikas, " + vardas + "! *";
+
+    int zvaigzdutes = eil3.length();
+     
+    for(int i=0;i<zvaigzdutes;i++)
+    {
+        eil1+="*";
+        eil5+="*";
+    }
+    for(int i=0;i<zvaigzdutes-2;i++)
+    {
+        eil2+=" ";
+        eil4+=" ";
+    }
+    eil2+="*";
+    eil4+="*";
+
+    cout << eil1 << endl << eil2 << endl << eil3 << endl << eil4 << endl << eil5;
+
+
+}
